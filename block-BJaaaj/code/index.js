3b9f62b8a5c0744329ecbd21455eb4371c1fb46c
harish8930 <129581462+harish8930@users.noreply.github.com>
2023-05-04 11:30:40 +0530

    Update index.js

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -1,41 +1,54 @@
-// Accessing and assigning.
-
-let firstName = "Arya";
-let lastName = "Stark";
-
-// Do the following:
-
-// Alert the value of the firstName.
-// Alert the value of the lastName.
-// Assign a new value of `"John"` to firstName.
-// Assign a new value of `"Snow"` to lastName.
-// Alert the new value of `firstName` and `lastName` by concatenating them
-// Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
-
-// Declare three variables `name,` `newUser` and `isAdmin`.
-// Assign your name as the value of `name`.
-// Assign the value of `name` to `newUser`.
-// Set the value of `isAdmin` to `true`.
-// Alert the values of all the three variables.
-
-// Assignment and re-assignment
-
-var wiseMan = "Tyrion Lannister";
-
-// Follow instructions and do the following. In case of an error write the error in comment
-
-// Reassign the value of `wiseMan` to "Samwell Tarly"
-// Declare a variable `userName` with value "Lysa Arryn"
-// Declare a variable named `oddNumber` and assign a value `57`.
-// Reassign the value of `oddNumber` to 61
-
-// Mathematical Operation
-
+let firstname ="arya"
+undefined
+let lastname ="stark"
+undefined
+alert(firstname)
+undefined
+alert(lastname)
+undefined
+let firstname = "john"
+undefined
+let lastname ="snow"
+undefined
+alert(firstname + "" + lastname)
+undefined
+ let name = 'Harish'
+undefined
+let newuser ='Harish'
+undefined
+let isadmin = 'true'
+undefined
+ 
+undefined
+alert( name + newuser + isadmin)
+undefined
+let wiseman = "samwell tarly"
+undefined
+let username ="lysa arryn"
+undefined
+let oddnumber="57"
+undefined
+let oddnumber="61"
+undefined
+oddnumber
+'61'
 let amount = 4280;
-
-// Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
-
-// Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
-// Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
-// Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
-// Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+undefined
+let newvariable= 4280-24
+undefined
+substraction
+VM2428:1 Uncaught ReferenceError: substraction is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM2428:1
+let addedamount = "4280+32"
+undefined
+sum
+150
+ let multipleamount = "4280*7"
+undefined
+multipleamount
+'4280*7'
+multiplication
+VM2647:1 Uncaught ReferenceError: multiplication is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM2647:1
